63fd5a82052b09169bfb02a0282249bf7c2f8d7e
josegibson <josegibson7@gmail.com>
2024-11-19 19:46:30 +0530

    Env variable name change

--- frontend/src/components/useAgent.js
+++ frontend/src/components/useAgent.js
@@ -1,17 +1,12 @@
-const API_URL = process.env.API_URL;
+const API_URL = process.env.REACT_APP_API_URL;
 
 export function useAgent() {
   const makeAgentMove = async (board, player, dropPiece, agent) => {
 
-    const response_ = await fetch(`${API_URL}/`);
-    const json = await response_.json();
-    console.log(json);
-
-
     const response = await fetch(`${API_URL}/api/move`, {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
+      "Content-Type": "application/json",
       },
       body: JSON.stringify({ board: board, player: player, agent: agent }),
     });
