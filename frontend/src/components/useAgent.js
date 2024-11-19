9df9941af8da11ccde9504442cd4267641d35f3d
josegibson <josegibson7@gmail.com>
2024-11-19 12:29:51 +0530

    Setup env variables

--- frontend/src/components/useAgent.js
+++ frontend/src/components/useAgent.js
@@ -1,6 +1,8 @@
+const API_URL = process.env.API_URL || "http://localhost:5000";
+
 export function useAgent() {
   const makeAgentMove = async (board, player, dropPiece, agent) => {
-    const response = await fetch("http://localhost:5000/api/move", {
+    const response = await fetch(`${API_URL}/api/move`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
