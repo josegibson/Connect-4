e216a47318401fab3da763b2b51b4e7b01b1ac97
josegibson <josegibson7@gmail.com>
2024-11-22 17:45:53 +0530

    More UI changes

--- frontend/src/components/PlayerPanel.js
+++ frontend/src/components/PlayerPanel.js
@@ -5,19 +5,30 @@ function PlayerPanel({ player, turn, status, onAgentChange, agent }) {
     <div className="player-panel">
       <div className={`display-tile player-${player} ${turn === player ? "active" : ""}`}></div>
       <div>
-        <label htmlFor={`difficulty-${player}`}>Played by </label>
-        <select
-          id={`played-by-${player}`}
-          value={agent}
-          onChange={(e) => {
-            onAgentChange(e, player);
-          }}
-        >
-          <option value="random">Random</option>
-          <option value="lookahead">Look Ahead</option>
-          <option value="human">Human</option>
-        </select>
-        <h2>
+        <div style={{ textAlign: 'start' }}>
+          <label htmlFor={`difficulty-${player}`} style={{ fontSize: 'small', margin: 0 }}>Played by </label>
+          <div>
+            <button 
+              className={agent === "random" ? "active-tab" : "outlined"} 
+              onClick={() => onAgentChange({ target: { value: "random" } }, player)}
+            >
+              Random
+            </button>
+            <button 
+              className={agent === "lookahead" ? "active-tab" : "outlined"} 
+              onClick={() => onAgentChange({ target: { value: "lookahead" } }, player)}
+            >
+              LookAhead
+            </button>
+            <button 
+              className={agent === "human" ? "active-tab" : "outlined"} 
+              onClick={() => onAgentChange({ target: { value: "human" } }, player)}
+            >
+              Human
+            </button>
+          </div>
+        </div>
+        <h2 style={{ margin: 0 }}>
           {status === player
             ? "You won!!"
             : status === (player === 1 ? 2 : 1)
