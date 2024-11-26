fdbd4ca865e6b4e6ec3511e5370c05ca69795010
josegibson <josegibson7@gmail.com>
2024-11-26 15:04:44 +0530

    serverless compactible

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -4,22 +4,12 @@ import { useAgent } from "./useAgent";
 import ConfigPanel from "./ConfigPanel";
 
 function App() {
-  const { makeAgentMove, updateConfig, getConfig } = useAgent();
+  const { makeAgentMove } = useAgent();
   const [config, setConfig] = useState({
     cols: 7,
     rows: 6,
     inARow: 4,
   });
-
-  useEffect(() => {
-    const init_config = getConfig();
-    setConfig((prev) => ({
-      ...prev,
-      ["cols"]: init_config.columns,
-      ["rows"]: init_config.rows,
-      ["inARow"]: init_config.inarow,
-    }));
-  }, []);
 
   const [playerAgents, setPlayerAgents] = useState({
     1: "human",
@@ -35,29 +25,25 @@ function App() {
   };
 
   const handleBoardSizeChange = (size) => {
-    // Implement the logic to change the board size
     const cols = parseInt(size.split("x")[1], 10);
     const rows = parseInt(size.split("x")[0], 10);
-    setConfig((prev) => ({ ...prev, ["rows"] : rows, ["cols"] : cols }));
+    setConfig((prev) => ({ ...prev, rows, cols }));
   };
 
   const handleInARowChange = (inARow) => {
-    // Implement the logic to change the in a row value
-    setConfig((prev) => ({ ...prev, ["inARow"]: inARow }));
+    setConfig((prev) => ({ ...prev, inARow: parseInt(inARow, 10) }));
   };
 
   useEffect(() => {
-    updateConfig(config);
     newGame();
   }, [config]);
 
   useEffect(() => {
-    console.log(playerAgents);
     if (status === 0 && player === 2) {
       if (playerAgents[2] === "human") {
         setUserTurn(true);
       } else {
-        makeAgentMove(board, player, dropPiece, playerAgents[2]);
+        makeAgentMove(playerAgents[2], config, board, player , dropPiece);
       }
     }
 
@@ -65,7 +51,7 @@ function App() {
       if (playerAgents[1] === "human") {
         setUserTurn(true);
       } else {
-        makeAgentMove(board, player, dropPiece, playerAgents[1]);
+        makeAgentMove(playerAgents[1], config, board, player, dropPiece);
       }
     }
   }, [status, player]);
--- frontend/src/components/useAgent.js
+++ frontend/src/components/useAgent.js
@@ -1,42 +1,24 @@
 const API_URL = process.env.REACT_APP_API_URL;
 
 export function useAgent() {
-  const makeAgentMove = async (board, player, dropPiece, agent) => {
-
-    const response = await fetch(`${API_URL}/api/move`, {
+  const makeAgentMove = async (agent, config, board, player, dropPiece) => {
+    const response = await fetch(`${API_URL}/move`, {
       method: "POST",
       headers: {
-      "Content-Type": "application/json",
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ board: board, player: player, agent: agent }),
+      body: JSON.stringify({"config": config, "board": board, "player": player, "agent": agent }),
     });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      console.error("Error making agent move:", errorData.error);
+      return;
+    }
 
     const data = await response.json();
     dropPiece(data.move);
   };
 
-  const updateConfig = async (config) => {
-    const response = await fetch(`${API_URL}/api/config`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({columns: config.cols, rows: config.rows, inarow: config.inARow}),
-    });
-
-    const data = await response.json();
-    console.log(data);
-  };
-
-  const getConfig = async () => {
-    const response = await fetch(`${API_URL}/api/config`, {
-      method: "GET",
-    });
-
-    const data = await response.json();
-    console.log(data);
-    return data;
-  };
-
-  return { makeAgentMove, updateConfig, getConfig };
+  return { makeAgentMove };
 }
